be06f10ee06cedf2c9115be32a3f6a020ca99932
Denis Coelho <deniscoelho.dev@gmail.com>
2023-04-06 23:15:29 -0300

    atualizacao

--- src/main/java/br/core/correios/repository/SetupRepository.java
+++ src/main/java/br/core/correios/repository/SetupRepository.java
@@ -1,6 +1,13 @@
 package br.core.correios.repository;
 
 import br.core.correios.model.Address;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,9 +15,31 @@ import java.util.List;
 
 @Repository
 public class SetupRepository {
-    public List<Address> getFromOrigin(){
-        return new ArrayList<>();
+
+    @Value("${correios.base.url}")
+    private String url;
+
+    public List<Address> getFromOrigin() throws Exception {
+        List<Address> resultList = new ArrayList<>();
+        String resultStr = "";
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpClient.execute(new HttpGet(this.url)))
+        {
+            HttpEntity entity = response.getEntity();
+            resultStr = EntityUtils.toString(entity);
+        }
+
+        String[] resultStrSplited = resultStr.split("\n");
+
+
+        for(String currentLine : resultStrSplited){
+            String[] currentLineSplited = currentLine.split(",");
+        }
+
+        return resultList;
     }
 
-    //47:03
+    //56:00 this...
+
 }
